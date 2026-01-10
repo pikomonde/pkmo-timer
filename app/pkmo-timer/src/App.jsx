2c9d7b1d0683598aba6e7d272036fc099fce37d3
pikomonde <32364823+pikomonde@users.noreply.github.com>
2026-01-10 23:37:53 +0700

    feat(phase2-ticking): implement the ticking

--- app/pkmo-timer/src/App.jsx
+++ app/pkmo-timer/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css'
 
+const MS_IN_60_FPS = 1000 / 60;
+
 function secondsToHMS(seconds) {
   return {
     hour: Math.floor(seconds / 3600),
@@ -9,56 +11,20 @@ function secondsToHMS(seconds) {
   };
 }
 
-const Timer = React.memo(function Timer({ 
+const Timer = React.memo(function Timer({
   id,
-  timer: {name, totalSeconds, status},
-  editingTimer,
+  timer: {name, totalSeconds, status, runSecondsLeft},
+  editingTimer = { name: '', hour: 0, minute: 0, second: 0 },
   isOtherTimerEdited,
-  setTimers,
   onUpdate,
   onEdit,
-  onDelete, 
+  onDelete,
+  onChange,
+  onStartTimer,
+  onStopTimer,
 }) {
-  const hms = secondsToHMS(totalSeconds);
-
-  const onChange = React.useCallback((event, fieldName) => {
-    let val = event.target.value;
-    switch (fieldName) {
-      case 'hour':
-        val = Math.floor(val)
-        if (val < 0) {
-          val = 0;
-        }
-        break;
-      case 'minute':
-        val = Math.floor(val)
-        if (val < 0) {
-          val = 0;
-        } else if (val > 59) {
-          val = 59;
-        }
-        break;
-      case 'second':
-        val = Math.floor(val)
-        if (val < 0) {
-          val = 0;
-        } else if (val > 59) {
-          val = 59;
-        }
-        break;
-    }
-
-    setTimers(prev => {
-      return {
-        ...prev,
-        editingTimer: {
-          ...prev.editingTimer,
-          [fieldName]: val,
-        },
-      }
-    });
-  }, [setTimers]);
-
+  const {hour, minute, second} = secondsToHMS(totalSeconds);
+  const {hour: runHour, minute: runMinute, second: runSecond} = secondsToHMS(runSecondsLeft);
 
   return (
     <div className='timer-card'>
@@ -66,35 +32,34 @@ const Timer = React.memo(function Timer({
         type='text'
         placeholder='Input timer name'
         disabled={status !== 'editing'}
-        value={status !== 'editing' ? name : editingTimer.name}
+        value={status === 'editing' ? editingTimer.name : name}
         onChange={(event) => onChange(event, 'name')}
       />
       <input
         type='number'
         placeholder='Input hours'
         disabled={status !== 'editing'}
-        value={status !== 'editing' ? hms.hour : editingTimer.hour}
+        value={status === 'editing' ? editingTimer.hour : (status === 'running' || status === 'notifying' ? runHour : hour)}
         onChange={(event) => onChange(event, 'hour')}
       />
       <input
         type='number'
         placeholder='Input minutes'
         disabled={status !== 'editing'}
-        value={status !== 'editing' ? hms.minute : editingTimer.minute}
+        value={status === 'editing' ? editingTimer.minute : (status === 'running' || status === 'notifying' ? runMinute : minute)}
         onChange={(event) => onChange(event, 'minute')}
       />
       <input
         type='number'
         placeholder='Input seconds'
         disabled={status !== 'editing'}
-        value={status !== 'editing' ? hms.second : editingTimer.second}
+        value={status === 'editing' ? editingTimer.second : (status === 'running' || status === 'notifying' ? runSecond : second)}
         onChange={(event) => onChange(event, 'second')}
       />
-      
+
       {status === 'editing' &&
         <button
           className='timer-button-save'
-          disabled={status !== 'editing'}
           onClick={() => onUpdate(id, editingTimer)}
         >
           Save
@@ -103,7 +68,6 @@ const Timer = React.memo(function Timer({
       {status === 'editing' &&
         <button
           className='timer-button-delete'
-          disabled={status !== 'editing'}
           onClick={() => onDelete(id)}
         >
           Delete
@@ -111,11 +75,37 @@ const Timer = React.memo(function Timer({
       }
       {status === 'idle' &&
         <button
+          className='timer-button-start'
+          disabled={isOtherTimerEdited}
+          onClick={() => onStartTimer(id)}
+          >
+          Start
+        </button>
+      }
+      {(status === 'idle') &&
+        <button
           className='timer-button-edit'
-          disabled={status !== 'idle' || isOtherTimerEdited}
+          disabled={isOtherTimerEdited}
           onClick={() => onEdit(id)}
         >
           Edit
+        </button>
+      }
+      {status === 'running' &&
+        <button
+          className='timer-button-stop'
+          disabled={isOtherTimerEdited}
+          onClick={() => onStopTimer(id)}
+        >
+          Stop
+        </button>
+      }
+      {(status === 'notifying') &&
+        <button
+          style={{backgroundColor: 'red'}}
+          onClick={() => onStopTimer(id)}
+        >
+          RED
         </button>
       }
 
@@ -128,7 +118,6 @@ function Timers({ timers, setTimers }) {
   const onCreate = React.useCallback(() => {
     const id = crypto.randomUUID();
     setTimers(prev => {
-      
       return {
         ...prev,
         byId: {
@@ -136,7 +125,9 @@ function Timers({ timers, setTimers }) {
           [id]: {
             name: '',
             totalSeconds: 0,
-            status: 'editing', // 'idle', 'editing', or 'running'
+            status: 'editing', // 'idle', 'editing', 'running', or 'notifying'
+            notifyAt: 0, // in mili unix time stamp, will be used when the timer starts
+            runSecondsLeft: 0,
           }
         },
         allIds: [...prev.allIds, id],
@@ -201,6 +192,44 @@ function Timers({ timers, setTimers }) {
     });
   }, [setTimers]);
 
+  const onChange = React.useCallback((event, fieldName) => {
+    let val = event.target.value;
+    switch (fieldName) {
+      case 'hour':
+        val = Math.floor(val)
+        if (val < 0) {
+          val = 0;
+        }
+        break;
+      case 'minute':
+        val = Math.floor(val)
+        if (val < 0) {
+          val = 0;
+        } else if (val > 59) {
+          val = 59;
+        }
+        break;
+      case 'second':
+        val = Math.floor(val)
+        if (val < 0) {
+          val = 0;
+        } else if (val > 59) {
+          val = 59;
+        }
+        break;
+    }
+
+    setTimers(prev => {
+      return {
+        ...prev,
+        editingTimer: {
+          ...prev.editingTimer,
+          [fieldName]: val,
+        },
+      }
+    });
+  }, [setTimers]);
+
   const onDelete = React.useCallback((id) => {
     setTimers(prev => {
       const { [id]: _, ...remainingById } = prev.byId;
@@ -221,6 +250,42 @@ function Timers({ timers, setTimers }) {
     });
   }, [setTimers]);
 
+  const onStartTimer = React.useCallback((id) => {
+    setTimers(prev => {
+      const totalSeconds = prev.byId[id].totalSeconds;
+      const totalMiliSeconds = 1000 * totalSeconds;
+      const notifyAt = Date.now() + totalMiliSeconds;
+      return {
+        ...prev,
+        byId: {
+          ...prev.byId,
+          [id]: {
+            ...prev.byId[id],
+            status: 'running',
+            notifyAt: notifyAt,
+            runSecondsLeft: Math.ceil((totalMiliSeconds) / 1000),
+          }
+        },
+      }
+    });
+  }, [setTimers]);
+
+  const onStopTimer = React.useCallback((id) => {
+    setTimers(prev => {
+      return {
+        ...prev,
+        byId: {
+          ...prev.byId,
+          [id]: {
+            ...prev.byId[id],
+            status: 'idle',
+            notifyAt: 0,
+          }
+        },
+      }
+    });
+  }, [setTimers]);
+
   return (
     <div className='timers-container'>
       {timers.allIds.map((id) => {
@@ -232,10 +297,12 @@ function Timers({ timers, setTimers }) {
           timer={timers.byId[id]}
           editingTimer={isEditingThisOne ? timers.editingTimer: null}
           isOtherTimerEdited={isOtherTimerEdited}
-          setTimers={setTimers}
           onUpdate={onUpdate}
           onEdit={onEdit}
+          onChange={onChange}
           onDelete={onDelete}
+          onStartTimer={onStartTimer}
+          onStopTimer={onStopTimer}
         />
       })}
       <button
@@ -257,20 +324,27 @@ function App() {
       [id1]: {
         name: "coffee break",
         totalSeconds: 5 * 60, // in seconds
-        status: 'idle',
+        status: 'idle', // 'idle', 'editing', 'running', or 'notifying'
+        notifyAt: 0, // in mili unix time stamp, will be used when the timer starts
+        runSecondsLeft: 0,
       },
       [id2]: {
         name: "generating report A",
         totalSeconds: 20 * 60, // in seconds
-        status: 'idle',
+        status: 'idle', // 'idle', 'editing', 'running', or 'notifying'
+        notifyAt: 0, // in mili unix time stamp, will be used when the timer starts
+        runSecondsLeft: 0,
       },
       [id3]: {
         name: "generating report B",
         totalSeconds: 12 * 60, // in seconds
-        status: 'idle',
+        status: 'idle', // 'idle', 'editing', 'running', or 'notifying'
+        notifyAt: 0, // in mili unix time stamp, will be used when the timer starts
+        runSecondsLeft: 0,
       }
     },
     allIds: [id1, id2, id3],
+    // TODO: how to make this class as default?
     editingTimer: {
       isEditing: false,
       name: '',
@@ -281,6 +355,41 @@ function App() {
   };
 
   const [timers, setTimers] = React.useState(exampleListOfTimers);
+  React.useEffect(() => {
+    const tick = setInterval(() => {
+      setTimers(prev => {
+        const now = Date.now();
+        const updatedTimers = {};
+        prev.allIds.map((id) => {
+          const timer = prev.byId[id];
+          if (timer.status === 'running') {
+            if (now + timer.totalSeconds > timer.notifyAt) {
+              updatedTimers[id] = {
+                ...timer,
+                status: 'notifying',
+                notifyAt: 0,
+                runSecondsLeft: 0,
+              };
+            } else {
+              updatedTimers[id] = {
+                ...timer,
+                runSecondsLeft: Math.ceil((timer.notifyAt - now) / 1000),
+              };
+            }
+          }
+        });
+        return {
+          ...prev,
+          byId: {
+            ...prev.byId,
+            ...updatedTimers,
+          },
+        }
+      });
+    }, MS_IN_60_FPS);
+
+    return () => clearInterval(tick);
+  }, []);
 
   return (
     <>
