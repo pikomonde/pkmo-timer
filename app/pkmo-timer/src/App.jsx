729a3800a2165f733417a7cc05a267de71790552
pikomonde <32364823+pikomonde@users.noreply.github.com>
2026-01-28 22:24:01 +0700

    feat: add css and styling

--- app/pkmo-timer/src/App.jsx
+++ app/pkmo-timer/src/App.jsx
@@ -58,98 +58,103 @@ const Timer = React.memo(function Timer({
         value={status === 'editing' ? editingTimer.name : name}
         onChange={(event) => onChange(event, 'name')}
       />
-      <input
-        type='number'
-        placeholder='Input hours'
-        disabled={status !== 'editing'}
-        value={status === 'editing' ? editingTimer.hour : (isDisplayRunningTime ? runHour : hour)}
-        onChange={(event) => onChange(event, 'hour')}
-      />
-      <input
-        type='number'
-        placeholder='Input minutes'
-        disabled={status !== 'editing'}
-        value={status === 'editing' ? editingTimer.minute : (isDisplayRunningTime ? runMinute : minute)}
-        onChange={(event) => onChange(event, 'minute')}
-      />
-      <input
-        type='number'
-        placeholder='Input seconds'
-        disabled={status !== 'editing'}
-        value={status === 'editing' ? editingTimer.second : (isDisplayRunningTime ? runSecond : second)}
-        onChange={(event) => onChange(event, 'second')}
-      />
+      <div className='timer-card-controls'>
+        <div className='timer-card-time-group'>
+          <input
+            type='number'
+            placeholder='Input hours'
+            disabled={status !== 'editing'}
+            value={status === 'editing' ? editingTimer.hour : (isDisplayRunningTime ? runHour : hour)}
+            onChange={(event) => onChange(event, 'hour')}
+          />
+          <input
+            type='number'
+            placeholder='Input minutes'
+            disabled={status !== 'editing'}
+            value={status === 'editing' ? editingTimer.minute : (isDisplayRunningTime ? runMinute : minute)}
+            onChange={(event) => onChange(event, 'minute')}
+          />
+          <input
+            type='number'
+            placeholder='Input seconds'
+            disabled={status !== 'editing'}
+            value={status === 'editing' ? editingTimer.second : (isDisplayRunningTime ? runSecond : second)}
+            onChange={(event) => onChange(event, 'second')}
+          />
+        </div>
 
-      {status === 'editing' &&
-        <button
-          className='timer-button-save'
-          onClick={() => onUpdate(id, editingTimer)}
-        >
-          Save
-        </button>
-      }
-      {status === 'editing' &&
-        <button
-          className='timer-button-delete'
-          onClick={() => onDelete(id)}
-        >
-          Delete
-        </button>
-      }
-      {status === 'idle' &&
-        <button
-          className='timer-button-start'
-          disabled={isOtherTimerEdited}
-          onClick={() => onStartTimer(id)}
-          >
-          Start
-        </button>
-      }
-      {(status === 'idle') &&
-        <button
-          className='timer-button-edit'
-          disabled={isOtherTimerEdited}
-          onClick={() => onEdit(id)}
-        >
-          Edit
-        </button>
-      }
-      {status === 'running' &&
-        <button
-          className='timer-button-pause'
-          disabled={isOtherTimerEdited}
-          onClick={() => onPauseTimer(id)}
-        >
-          Pause
-        </button>
-      }
-      {status === 'paused' &&
-        <button
-          className='timer-button-resume'
-          disabled={isOtherTimerEdited}
-          onClick={() => onResumeTimer(id)}
-        >
-          Resume
-        </button>
-      }
-      {status === 'paused' &&
-        <button
-          className='timer-button-stop'
-          disabled={isOtherTimerEdited}
-          onClick={() => onStopTimer(id)}
-        >
-          Stop
-        </button>
-      }
-      {(status === 'notifying') &&
-        <button
-          style={{backgroundColor: 'red'}}
-          onClick={() => onStopTimer(id)}
-        >
-          RED
-        </button>
-      }
-
+        <div className='timer-card-action-group'>
+          {status === 'editing' &&
+            <button
+              className='timer-card-button timer-button-save'
+              onClick={() => onUpdate(id, editingTimer)}
+            >
+              Save
+            </button>
+          }
+          {status === 'editing' &&
+            <button
+              className='timer-card-button timer-button-delete'
+              onClick={() => onDelete(id)}
+            >
+              Del
+            </button>
+          }
+          {status === 'idle' &&
+            <button
+              className='timer-card-button timer-button-start'
+              disabled={isOtherTimerEdited}
+              onClick={() => onStartTimer(id)}
+              >
+              Start
+            </button>
+          }
+          {(status === 'idle') &&
+            <button
+              className='timer-card-button timer-button-edit'
+              disabled={isOtherTimerEdited}
+              onClick={() => onEdit(id)}
+            >
+              Edit
+            </button>
+          }
+          {status === 'running' &&
+            <button
+              className='timer-card-button timer-button-pause'
+              disabled={isOtherTimerEdited}
+              onClick={() => onPauseTimer(id)}
+            >
+              Pause
+            </button>
+          }
+          {status === 'paused' &&
+            <button
+              className='timer-card-button timer-button-resume'
+              disabled={isOtherTimerEdited}
+              onClick={() => onResumeTimer(id)}
+            >
+              Rsme
+            </button>
+          }
+          {status === 'paused' &&
+            <button
+              className='timer-card-button timer-button-stop'
+              disabled={isOtherTimerEdited}
+              onClick={() => onStopTimer(id)}
+            >
+              Restart
+            </button>
+          }
+          {(status === 'notifying') &&
+            <button
+              className='timer-card-button timer-button-notify'
+              onClick={() => onStopTimer(id)}
+            >
+              Stop
+            </button>
+          }
+        </div>
+      </div>
     </div>
   )
 });
